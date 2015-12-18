e172e359c71738d222bb72ce6a79eb4510ca04e8
Shawn Fiske <durah_shar@hotmail.com>
2015-12-18 10:16:40 -0800

    added descriptions to the play ground section

--- app/js/controllers/playground_controller.js
+++ app/js/controllers/playground_controller.js
@@ -3,6 +3,12 @@
 module.exports = function(app) {
 	app.controller('playgroundController', ['$scope', '$http', function($scope, $http) {
 		$scope.projects = [];
+
+		$scope.hover = function(project) {
+				// Shows/hides the delete button on hover
+				return project.showInfo = ! project.showInfo;
+		};
+		
     $http.get('data/playground.json').success(function(data) {
       $scope.projects = data.data;
     });
